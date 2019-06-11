1b9c64f9c6609d746ff19b6351b4c3e8ece3e01f
Shain Singh <shainsingh89@gmail.com>
2019-06-11 14:23:29 +0200

    Androidx migration

--- packages/video_vlc/android/src/main/java/com/fidelisa/videovlc/VideoActivity.java
+++ packages/video_vlc/android/src/main/java/com/fidelisa/videovlc/VideoActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -145,7 +145,7 @@ public class VideoActivity extends AppCompatActivity implements IVLCVout.Callbac
   @Override
   protected void onDestroy() {
     super.onDestroy();
-    releasePlayer();
+    //releasePlayer();
   }
 
   @Override
